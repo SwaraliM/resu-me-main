37b64cbe618783a5e4421dc2f55816b6f1d9675b
SwaraliM <mahimkarswarali@gmail.com>
2022-07-05 17:04:10 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React, { Component } from "react";
+import ResumeForm from "./components/ResumeForm";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ResumeForm />
+      </div>
+    );
+  }
+}
+
+export default App;
